b74209f84595ef1b62987c345c7eb843a60a5c42
Leonardo Miranda <leonardomisi@hotmail.com>
2021-04-20 20:40:48 -0300

    Second day of NLW 05 - Maximum Speed

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,14 @@
 import express from "express";
 
+import "./database";
+import { routes } from "./routes";
+
 const app = express();
 
-app.get("/", (req, res) => {
-  return res.json({ message: "Olá, NLW 05" });
-});
+app.use(express.json());
 
-app.post("/", (req, res) => {
-  return res.json({ message: "Usuário salvo com sucesso" });
-});
+app.use(routes);
 
 app.listen(3333, () => {
-  console.log("Server running on port 3333");
+    console.log("Server running on port 3333");
 });
